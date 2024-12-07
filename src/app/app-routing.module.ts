a3c6d4fd0be51967ecbb00d6b947d0b2f286007c
mahamfathy <mahafathyh@gmail.com>
2024-12-07 17:49:47 +0200

    init the app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'manager', loadChildren: () => import('./features/dashboard/manager/manager.module').then(m => m.ManagerModule) }, { path: 'employee', loadChildren: () => import('./features/dashboard/employee/employee.module').then(m => m.EmployeeModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
