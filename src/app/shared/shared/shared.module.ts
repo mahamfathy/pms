ac324b20845e659c42dda18bd1b2b313dc9bd653
mahamfathy <mahafathyh@gmail.com>
2024-12-07 18:29:43 +0200

    [docs] sharedModule : update sharedmodule with forms and routermodule

--- src/app/shared/shared/shared.module.ts
+++ src/app/shared/shared/shared.module.ts
@@ -1,12 +1,10 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, RouterModule, FormsModule, ReactiveFormsModule],
 })
-export class SharedModule { }
+export class SharedModule {}
