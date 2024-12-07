32009371d8365cf99427c844c770482fc0bdeda2
mahamfathy <mahafathyh@gmail.com>
2024-12-08 01:21:09 +0200

    [feat] interceptor: create global interceptor

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,9 +1,11 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-
-  constructor() { }
+  constructor(private _HttpClient: HttpClient) {}
+  onLogin(): Observable<any> {}
 }
---
+++ src/app/core/interceptors/global.interceptor.ts
@@ -0,0 +1,28 @@
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class GlobalInterceptor implements HttpInterceptor {
+  constructor() {}
+
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
+    const baseUrl = 'https://upskilling-egypt.com:3003/api/v1/';
+    const token = localStorage.getItem('userToken');
+    const newRequest = request.clone({
+      url: `${baseUrl}${request.url}`,
+      setHeaders: {
+        Authorization: `${token}`,
+      },
+    });
+    return next.handle(newRequest);
+  }
+}
