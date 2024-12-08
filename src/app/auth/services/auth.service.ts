688b72854a7fd98dcb1baa94a2557ff8dba88df3
mahamfathy <mahafathyh@gmail.com>
2024-12-08 02:34:57 +0200

    [feat] service: create login method in auth service

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,11 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { ILogin } from '../interfaces/ILogin';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   constructor(private _HttpClient: HttpClient) {}
-  onLogin(): Observable<any> {}
+  onLogin(loginForm: ILogin): Observable<any> {
+    return this._HttpClient.post('Users/Login', loginForm);
+  }
 }
