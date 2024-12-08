518e7b7df537b651b514744c0b6b8f6ac64e52c3
mahamfathy <mahafathyh@gmail.com>
2024-12-08 03:07:28 +0200

    [feat] component: create reset password component and fix tthe routes

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ResetRequestComponent } from './components/reset-request/reset-request.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'reset-request', component: ResetRequestComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AuthRoutingModule { }
+export class AuthRoutingModule {}
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
+import { ResetRequestComponent } from './components/reset-request/reset-request.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ResetRequestComponent
+  ],
   imports: [
     CommonModule,
     AuthRoutingModule
