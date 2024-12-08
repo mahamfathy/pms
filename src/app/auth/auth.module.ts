28959981c7cd5e386ee1630882a70dd9378d41f5
mahamfathy <mahafathyh@gmail.com>
2024-12-08 03:17:07 +0200

    [docs]module:update shared module and auth module to contain mat-form and other angular material component

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,17 +1,12 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
+import { SharedModule } from '../shared/shared/shared.module';
 import { AuthRoutingModule } from './auth-routing.module';
 import { ResetRequestComponent } from './components/reset-request/reset-request.component';
 
-
 @NgModule({
-  declarations: [
-    ResetRequestComponent
-  ],
-  imports: [
-    CommonModule,
-    AuthRoutingModule
-  ]
+  declarations: [ResetRequestComponent],
+  imports: [CommonModule, AuthRoutingModule, SharedModule],
 })
-export class AuthModule { }
+export class AuthModule {}
