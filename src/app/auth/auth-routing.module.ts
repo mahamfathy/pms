44e538d847b730d820a6412e9573420f0b325986
Hossam <hossamabdo7359@gmail.com>
2024-12-08 13:04:28 +0200

    [feat] component: add register

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ResetRequestComponent } from './components/reset-request/reset-request.component';
+import { RegisterComponent } from './components/register/register.component';
 
 const routes: Routes = [
   { path: 'reset-request', component: ResetRequestComponent },
+  { path: 'register', component: RegisterComponent },
 ];
 
 @NgModule({
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared/shared.module';
 import { AuthRoutingModule } from './auth-routing.module';
 import { ResetRequestComponent } from './components/reset-request/reset-request.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
-  declarations: [ResetRequestComponent],
+  declarations: [ResetRequestComponent, RegisterComponent],
   imports: [CommonModule, AuthRoutingModule, SharedModule],
 })
 export class AuthModule {}
