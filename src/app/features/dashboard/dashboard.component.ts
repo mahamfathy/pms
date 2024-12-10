8bcc3f90a5301186e48933511ee52bf804931680
mahamfathy <mahafathyh@gmail.com>
2024-12-11 00:02:16 +0200

    [feat] component sidebar : fininsh html and styling and ts

--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent {
-
+  isSidebarExpanded = true;
+  onToggleSidebar(expanded: boolean) {
+    this.isSidebarExpanded = expanded;
+  }
 }
