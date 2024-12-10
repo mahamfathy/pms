435d9411423f7827a9995a5c0fb965d0867819de
mahamfathy <mahafathyh@gmail.com>
2024-12-10 03:08:21 +0200

    [feat] component navbar : fininsh html and styling

--- src/app/features/dashboard/manager/manager-routing.module.ts
+++ src/app/features/dashboard/manager/manager-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from '../dashboard.component';
 import { ManagerComponent } from './manager.component';
 
-const routes: Routes = [{ path: '', component: ManagerComponent }];
+const routes: Routes = [
+  { path: '', component: ManagerComponent },
+  { path: 'home', component: DashboardComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class ManagerRoutingModule { }
+export class ManagerRoutingModule {}
--- src/app/features/dashboard/manager/manager.module.ts
+++ src/app/features/dashboard/manager/manager.module.ts
@@ -1,12 +1,13 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
+import { SharedModule } from 'src/app/shared/shared.module';
 import { ManagerRoutingModule } from './manager-routing.module';
 import { ManagerComponent } from './manager.component';
 import { ManagerProjectsComponent } from './components/manager-projects/manager-projects.component';
 
-
 @NgModule({
+<<<<<<< HEAD
   declarations: [
     ManagerComponent,
     ManagerProjectsComponent
@@ -15,5 +16,9 @@ import { ManagerProjectsComponent } from './components/manager-projects/manager-
     CommonModule,
     ManagerRoutingModule
   ]
+=======
+  declarations: [ManagerComponent],
+  imports: [CommonModule, ManagerRoutingModule, SharedModule],
+>>>>>>> 9690301 ([feat] component navbar : fininsh html and styling)
 })
-export class ManagerModule { }
+export class ManagerModule {}
