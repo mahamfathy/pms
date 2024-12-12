800df80fdaca16ad056b24ca89fe2aa534cabcbb
mahamfathy <mahafathyh@gmail.com>
2024-12-12 22:34:40 +0200

    [feat] users service :create the service and create get all users method

---
+++ src/app/features/dashboard/manager/modules/users/services/users.service.ts
@@ -0,0 +1,13 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UsersService {
+  constructor(private _HttpClient: HttpClient) {}
+  getAllUsers(): Observable<any> {
+    return this._HttpClient.get('Users');
+  }
+}
