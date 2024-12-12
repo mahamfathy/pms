53efa5ff4a7400481c401b7b75a217e7e75b8feb
assmabaziz <assma.baziz@yahoo.com>
2024-12-12 22:15:57 +0300

    style: Code style changes and routing added

--- src/app/features/dashboard/manager/modules/manager-projects/components/create-new-project/create-new-project.component.ts
+++ src/app/features/dashboard/manager/modules/manager-projects/components/create-new-project/create-new-project.component.ts
@@ -2,25 +2,39 @@ import { Component } from '@angular/core';
 import { ManagerProjectsService } from '../../services/manager-projects.service';
 import { IProject } from '../../interfaces/iproject';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-new-project',
   templateUrl: './create-new-project.component.html',
-  styleUrls: ['./create-new-project.component.scss']
+  styleUrls: ['./create-new-project.component.scss'],
 })
 export class CreateNewProjectComponent {
-constructor(private _ManagerProjectsService : ManagerProjectsService){}
-createNewProjectForm = new FormGroup({
-  title: new FormControl('', [Validators.required]),
-  description: new FormControl('')
-});
-createNewProject(infoProject: FormGroup) {
-  this._ManagerProjectsService.onCreateNewProject(infoProject.value).subscribe({
-    next:(res) => {
-      console.log(res);
-    }, error :(err) => {
-      console.log(err);
-    }
-  })
-}
+  constructor(
+    private _ManagerProjectsService: ManagerProjectsService,
+    private _ToastrService: ToastrService,
+    private _Router : Router
+  ) {}
+  createNewProjectForm = new FormGroup({
+    title: new FormControl('', [Validators.required]),
+    description: new FormControl(''),
+  });
+  createNewProject(infoProject: FormGroup) {
+    console.log(infoProject.value);
+    this._ManagerProjectsService
+      .onCreateNewProject(infoProject.value)
+      .subscribe({
+        next: (res) => {
+          console.log(res);
+        },
+        error: (err) => {
+          console.log(err);
+        },
+        complete: () => {
+          this._ToastrService.success('project added successfully')
+          this._Router.navigate(['../'])
+        },
+      });
+  }
 }
