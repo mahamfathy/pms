36913d5aa74ef05aa3d2c8f22b494a55244a9643
mahamfathy <mahafathyh@gmail.com>
2024-12-13 15:57:16 +0200

    [feat] view-user : create view user component

--- src/app/features/dashboard/manager/modules/users/users-routing.module.ts
+++ src/app/features/dashboard/manager/modules/users/users-routing.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AddEditUserComponent } from './components/add-edit-user/add-edit-user.component';
+import { AddUserComponent } from './components/add-user/add-user.component';
 import { UsersComponent } from './users.component';
 
 const routes: Routes = [
   { path: '', component: UsersComponent },
-  { path: 'add', component: AddEditUserComponent },
-  {
-    path: 'edit',
-    component: AddEditUserComponent,
-  },
+  { path: 'add', component: AddUserComponent },
 ];
 
 @NgModule({
--- src/app/features/dashboard/manager/modules/users/users.module.ts
+++ src/app/features/dashboard/manager/modules/users/users.module.ts
@@ -2,12 +2,13 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { SharedModule } from 'src/app/shared/shared.module';
-import { AddEditUserComponent } from './components/add-edit-user/add-edit-user.component';
+import { AddUserComponent } from './components/add-user/add-user.component';
 import { UsersRoutingModule } from './users-routing.module';
 import { UsersComponent } from './users.component';
+import { ViewUserComponent } from './components/view-user/view-user.component';
 
 @NgModule({
-  declarations: [UsersComponent, AddEditUserComponent],
+  declarations: [UsersComponent, AddUserComponent, ViewUserComponent],
   imports: [CommonModule, UsersRoutingModule, SharedModule],
 })
 export class UsersModule {}
