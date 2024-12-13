d7edee76f5fab3eb8f6b7a89fe260684bf986ccd
mahamfathy <mahafathyh@gmail.com>
2024-12-13 18:14:05 +0200

    [style] add-manager : finish styling

--- src/app/features/dashboard/manager/modules/users/users-routing.module.ts
+++ src/app/features/dashboard/manager/modules/users/users-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AddUserComponent } from './components/add-user/add-user.component';
+import { AddManagerComponent } from './components/add-manager/add-manager.component';
 import { UsersComponent } from './users.component';
 
 const routes: Routes = [
   { path: '', component: UsersComponent },
-  { path: 'add', component: AddUserComponent },
+  { path: 'add-manager', component: AddManagerComponent },
 ];
 
 @NgModule({
