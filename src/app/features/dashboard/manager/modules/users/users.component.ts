ddbb5114b45103dd3316eb60f45e74fa6218c27b
mahamfathy <mahafathyh@gmail.com>
2024-12-13 16:37:20 +0200

    [feat] Users : create getallusers method

--- src/app/features/dashboard/manager/modules/users/users.component.ts
+++ src/app/features/dashboard/manager/modules/users/users.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IUser } from './interfaces/IUser';
+import { UsersService } from './services/users.service';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrls: ['./users.component.scss']
+  styleUrls: ['./users.component.scss'],
 })
-export class UsersComponent {
-
+export class UsersComponent implements OnInit {
+  sourceData: IUser[] = [];
+  displayedColumns: string[] = [];
+  constructor(private _UsersService: UsersService) {}
+  ngOnInit(): void {
+    this.getAllUsers();
+  }
+  private getAllUsers(): void {
+    this._UsersService.getAllUsers().subscribe({
+      next: (res) => {
+        this.sourceData = res.data;
+      },
+    });
+  }
 }
