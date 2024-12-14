4eb0ad76e5ecf01e20ce292300552a43e127e2d5
Hossam <hossamabdo7359@gmail.com>
2024-12-14 06:07:20 +0200

    [feat] table: add pagination and search to table

--- src/app/shared/components/table/table.component.ts
+++ src/app/shared/components/table/table.component.ts
@@ -1,4 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewChild } from '@angular/core';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { TasksService } from 'src/app/features/dashboard/manager/modules/tasks/services/tasks.service';
 
 @Component({
   selector: 'app-table',
@@ -6,13 +9,65 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./table.component.scss'],
 })
 export class TableComponent {
-  @Input() dataSource: any[] = [];
+  @Input() dataSource!: any[];
   @Input() displayedColumns: string[] = [];
   @Input() actions: any[] = [];
-  constructor() {}
+  @Input() numRows!: number;
+  constructor(private _TasksService: TasksService) {}
+
+  data!: any;
+  filterName: string = 'Title';
+  pageSize: number = 5;
+  pageNumber: number = 1;
+  searchName: string = '';
 
   ngOnChanges(): void {
     console.log(this.displayedColumns);
     console.log(this.dataSource);
+    this.data = new MatTableDataSource(this.dataSource);
+  }
+
+  fireFilteration() {
+    let myparms = {
+      status: this.searchName,
+      pageNumber: this.pageNumber,
+      pageSize: this.pageSize,
+    };
+
+    this._TasksService.getAllTasks(myparms).subscribe({
+      next: (res) => {
+        console.log(res);
+        this.data = res.data;
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
+
+  filterChange(name: string) {
+    this.filterName = name;
+    this.searchName = '';
+    let myparms = {
+      pageNumber: this.pageNumber,
+      pageSize: this.pageSize,
+    };
+
+    this._TasksService.getAllTasks(myparms).subscribe({
+      next: (res) => {
+        console.log(res);
+        this.data = res.data;
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
+
+  handlePageEvent(e: PageEvent) {
+    // this.length = e.length;
+    this.pageSize = e.pageSize;
+    this.pageNumber = e.pageIndex + 1;
+    this.fireFilteration();
   }
 }
