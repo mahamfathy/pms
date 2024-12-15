5b45c55c4c4ecb59c5f06d6f8e868853aecec9e7
mahamfathy <mahafathyh@gmail.com>
2024-12-15 16:37:19 +0200

    edits

--- src/app/features/dashboard/manager/modules/users/users.module.ts
+++ src/app/features/dashboard/manager/modules/users/users.module.ts
@@ -3,13 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { SharedModule } from 'src/app/shared/shared.module';
 import { AddManagerComponent } from './components/add-manager/add-manager.component';
+import { BlockUserComponent } from './components/block-user/block-user.component';
 import { ViewUserComponent } from './components/view-user/view-user.component';
 import { UsersRoutingModule } from './users-routing.module';
 import { UsersComponent } from './users.component';
-import { BlockUserComponent } from './components/block-user/block-user.component';
 
 @NgModule({
-  declarations: [UsersComponent, ViewUserComponent, AddManagerComponent, BlockUserComponent],
+  declarations: [
+    UsersComponent,
+    ViewUserComponent,
+    AddManagerComponent,
+    BlockUserComponent,
+  ],
   imports: [CommonModule, UsersRoutingModule, SharedModule],
 })
 export class UsersModule {}
