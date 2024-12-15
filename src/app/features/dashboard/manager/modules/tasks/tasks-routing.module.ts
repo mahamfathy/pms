507704fe66479e688f655fc12421063552f8f9c7
Hossam <hossamabdo7359@gmail.com>
2024-12-15 17:04:01 +0200

    [feat] component :create add task component with design

--- src/app/features/dashboard/manager/modules/tasks/tasks-routing.module.ts
+++ src/app/features/dashboard/manager/modules/tasks/tasks-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TasksComponent } from './tasks.component';
+import { AddTaskComponent } from './components/add-task/add-task.component';
 
-const routes: Routes = [{ path: '', component: TasksComponent }];
+const routes: Routes = [
+  { path: '', component: TasksComponent },
+  { path: 'add-task', component: AddTaskComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class TasksRoutingModule { }
+export class TasksRoutingModule {}
