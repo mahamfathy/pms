2ece2859e9e99f7cdf64bfd982ef5d4df7538aac
mahamfathy <mahafathyh@gmail.com>
2024-12-15 13:09:17 +0200

    [feat] block-user component : create the component

--- src/app/features/dashboard/manager/modules/users/users.module.ts
+++ src/app/features/dashboard/manager/modules/users/users.module.ts
@@ -6,9 +6,10 @@ import { AddManagerComponent } from './components/add-manager/add-manager.compon
 import { ViewUserComponent } from './components/view-user/view-user.component';
 import { UsersRoutingModule } from './users-routing.module';
 import { UsersComponent } from './users.component';
+import { BlockUserComponent } from './components/block-user/block-user.component';
 
 @NgModule({
-  declarations: [UsersComponent, ViewUserComponent, AddManagerComponent],
+  declarations: [UsersComponent, ViewUserComponent, AddManagerComponent, BlockUserComponent],
   imports: [CommonModule, UsersRoutingModule, SharedModule],
 })
 export class UsersModule {}
