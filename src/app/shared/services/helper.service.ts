cad63bb35578ed22e80993d6e5668a94f578d853
mahamfathy <mahafathyh@gmail.com>
2024-12-16 15:28:42 +0200

    [feat] helper service : create observable for username email and image

--- src/app/shared/services/helper.service.ts
+++ src/app/shared/services/helper.service.ts
@@ -1,13 +1,20 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { IChangepassword } from 'src/app/auth/interfaces/ichangepassword';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HelperService {
+  private userNameSub = new BehaviorSubject<string | null>(null);
+  $userName = this.userNameSub.asObservable();
+  private emailSub = new BehaviorSubject<string | null>(null);
+  $email = this.emailSub.asObservable();
+  private imagePathSub = new BehaviorSubject<string | null>(null);
+  $imagePath = this.imagePathSub.asObservable();
   constructor(private _HttpClient: HttpClient) {}
+
   onGetCurrentUser(): Observable<any> {
     return this._HttpClient.get('Users/currentUser');
   }
@@ -18,10 +25,10 @@ export class HelperService {
   onChangePassword(changePasswordForm: IChangepassword): Observable<any> {
     return this._HttpClient.put('Users/ChangePassword', changePasswordForm);
   }
-  OnGetUserCount():Observable<any>{
-    return this._HttpClient.get('Users/count')
+  OnGetUserCount(): Observable<any> {
+    return this._HttpClient.get('Users/count');
   }
-  onTsksCount():Observable<any>{
-    return this._HttpClient.get('Task/count')
+  onTsksCount(): Observable<any> {
+    return this._HttpClient.get('Task/count');
   }
 }
