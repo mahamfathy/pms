3a2a15179170a38b59fc96e1cc5c67155ce91eba
mahamfathy <mahafathyh@gmail.com>
2024-12-16 15:35:09 +0200

    [feat] helper service : create getcurrentuser method

--- src/app/shared/services/helper.service.ts
+++ src/app/shared/services/helper.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
 import { IChangepassword } from 'src/app/auth/interfaces/ichangepassword';
 
 @Injectable({
@@ -11,12 +11,22 @@ export class HelperService {
   $userName = this.userNameSub.asObservable();
   private emailSub = new BehaviorSubject<string | null>(null);
   $email = this.emailSub.asObservable();
-  private imagePathSub = new BehaviorSubject<string | null>(null);
+  private imagePathSub = new BehaviorSubject<string | null>('');
   $imagePath = this.imagePathSub.asObservable();
   constructor(private _HttpClient: HttpClient) {}
 
   onGetCurrentUser(): Observable<any> {
-    return this._HttpClient.get('Users/currentUser');
+    return this._HttpClient.get('Users/currentUser').pipe(
+      tap((res: any) => {
+        if (res) {
+          this.userNameSub.next(res.userName);
+          this.emailSub.next(res.email);
+          this.imagePathSub.next(
+            res.imagePath || 'assets/images/def-avatar.avif'
+          );
+        }
+      })
+    );
   }
   //may be we will not use this function
   onGetAllProjects(data: any): Observable<any> {
