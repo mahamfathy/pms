67fc74fdc71e636fbfec9649ef7ba3e2a8c5d7ec
assmabaziz <assma.baziz@yahoo.com>
2024-12-17 20:37:44 +0300

    feat: new module added with some changes in style and routing

--- src/app/features/dashboard/employee/employee-routing.module.ts
+++ src/app/features/dashboard/employee/employee-routing.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EmployeeComponent } from './employee.component';
 
-const routes: Routes = [{ path: '', component: EmployeeComponent }];
+const routes: Routes = [
+  { path: '', component: EmployeeComponent },
+  {
+    path: 'user-tasks',
+    loadChildren: () =>
+      import('./modules/users-tasks/users-tasks.module').then(
+        (m) => m.UsersTasksModule
+      ),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class EmployeeRoutingModule { }
+export class EmployeeRoutingModule {}
